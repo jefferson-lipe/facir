53153e5ca5b1b84c03dda6a03ef50324bb5b42a8
jefferson-lipe <55898937+jefferson-lipe@users.noreply.github.com>
2019-10-25 23:07:15 -0300

    Create l6q14.c

---
+++ lista6resolvida/l6q14.c
@@ -0,0 +1,11 @@
+# include  < stdio.h >
+# include  < string.h >
+int  main () {
+int n;
+nome do caractere [ 50 ];
+printf ( " digite sua palavra: " );
+gets (nome);
+para (n = 0 ; n < strlen (nome); n ++) nome [n] + = 1 ;
+
+printf ( " comando sua palavra na tabela ASCII \ n ... \ n ... \ n ... \ n ... \ n o resultado e: % s " , nome);
+}
